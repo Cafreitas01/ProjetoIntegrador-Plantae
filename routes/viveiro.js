c7b12fdd765a617083bbac4be325ff5a2a5f7061
Leila Alves <leila.ae@gmail.com>
2022-03-17 23:35:36 -0300

    criando rotas

--- routes/viveiro.js
+++ routes/viveiro.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const viveiroController = require('../controllers/viveiroController');
-const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    res.render('viveiro');
+var express = require("express");
+var router = express.Router();
+var viveiroController = require("../controllers/viveiroController");
+
+/* GET home page. */
+router.get("/", function (req, res) {
+    res.render("viveiro", { title: "express" });
 });
 
-module.exports = viveiroController;
-
+module.exports = router;
