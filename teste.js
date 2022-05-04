d0584fb2f3f410552c58ab91671a9be38ab31834
carolina <cafreitas01@gmail.com>
2022-05-04 20:48:57 -0300

    sequelize

--- teste.js
+++ teste.js
@@ -1,8 +1,19 @@
-const { usuario} = require("./database/models")
+require("dotenv").config();
 
-usuario.findAll().then (response => console.log(response));
+const { Usuario} = require("./database/models");
+
+//Usuario.findAll().then (response => console.log(response));
 
 
+
+async function buscarUsuarios (){
+    const usuarios = await Usuarios.findAll({
+        include:"carinho_sessao"
+    }).then(usuarios => usuarios.map(usuario => usuario.toJSON()));
+    console.log(JSON.stringify(usuarios,null,4))
+}
+
+buscarUsuarios();
 
 //para importar mais tabela é so colocar virgula e colocar 
 //as models dentro da const{Usuario,blabla}
