e65bcfaa27f463ddb9ae8b626f3ca772c358b8fa
José Alexandre dos Santos <65632695+Alexandre2Santos@users.noreply.github.com>
2022-05-27 19:31:57 -0300

    shopping-cart-

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const bodyParser = require("body-parser");
 const contatoRouter = require('./routes/contato');
 const indexRouter = require('./routes/index');
 const pagamentoRouter = require('./routes/pagamento');
+const agradecimentoRouter = require('./routes/agradecimento')
 const carrinhoRouter = require('./routes/carrinho');
+const checkoutRouter = require('./routes/checkout');
 const sobreRouter = require('./routes/sobre');
 const terrariosRouter = require('./routes/terrarios');
 const viveiroRouter = require('./routes/viveiro');
@@ -28,6 +30,7 @@ const ferramentasRouter = require('./routes/ferramentas');
 const entregaRouter = require('./routes/entrega');
 
 const autenticador = require('./middlewares/autenticacao');
+const checkoutController = require('./controllers/checkoutController');
 
 const app = express();
 const usuario = require("./models/usuarios");
@@ -51,6 +54,8 @@ app.use(indexRouter);
 app.use('/viveiro', viveiroRouter);
 app.use('/terrarios', terrariosRouter);
 app.use('/carrinho', carrinhoRouter);
+app.use('/checkout',checkoutRouter);
+app.use('/agradecimento', agradecimentoRouter);
 app.use('/login', loginRouter);
 app.use('/pagamento', pagamentoRouter);
 app.use('/sobre', sobreRouter);
---
+++ controllers/checkoutController.js
@@ -0,0 +1,5 @@
+const checkoutController = {
+    index: (req, res) => res.render('checkout')
+  };
+  
+  module.exports = checkoutController;
